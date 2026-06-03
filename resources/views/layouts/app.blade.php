9afdada1aa2f7d91a2019a04cd5b70f9469585db
Jagdish Kumar Sarma <jagdish.sarma@nw18.com>
2026-06-03 12:01:48 +0530

    defer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,9 +29,9 @@
 </head>
 <body>
   <!-- jQuery -->
-  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <!-- Owl Carousel -->
-  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
   @yield('body')
   <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
   <script>
