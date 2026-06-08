265b44f24e012d710cc747e40da3c05410778813
findkuldeepnath-crypto <findkuldeepnath@gmail.com>
2026-06-08 18:20:04 +0530

    test

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'AutoTerra')
 @section('body')
 @include('partials.nav')
-<section style="background:var(--navy);padding:60px;"><h1 style="font-size:34px;font-weight:800;color:#fff;">{{ ucfirst(str_replace('-', ' ', request()->path())) }}</h1><p style="font-size:15px;color:rgba(210,230,248,0.5);margin-top:12px;">This page is under development.</p></section>
-@include('partials.footer')
+<section class="res-hero">
+  <div class="sec-eye">{{ pageContent('resources', 'resources.hero_eyebrow') }}</div>
+  <h1>{{ pageContent('resources', 'resources.hero_heading') }}</h1>
+  <p>{{ pageContent('resources', 'resources.hero_description') }}</p>
+</section>@include('partials.footer')
 @endsection
