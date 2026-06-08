8338df0ccbe4dfe71b3d86c26f49b4a0ef83f7b1
jayantatamuli86-create <jayantatamuli86@gmail.com>
2026-06-08 11:45:40 +0530

    test

--- resources/views/pages/eula.blade.php
+++ resources/views/pages/eula.blade.php
@@ -9,7 +9,7 @@
   <div class="legal-hero-inner">
     <div class="sec-eye" style="margin-bottom:12px;">{{ pageContent('eula', 'hero.pil_text') }}</div>
     <h1>{!! pageContent('eula', 'hero.heading') !!}</h1>
-    <p class="legal-hero-meta">{{ pageContent('eula', 'hero.sub_heading') }}</p>
+    <p class="legal-hero-meta">{!! pageContent('eula', 'hero.sub_heading') !!}</p>
   </div>
 </section>
 <!-- eula hero section end -->
