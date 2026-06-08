4eabfcbbfa2a4931441e8c58e9413f4bec41e2cc
findkuldeepnath-crypto <findkuldeepnath@gmail.com>
2026-06-08 18:27:50 +0530

    test2

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -4,7 +4,8 @@
 @include('partials.nav')
 <section class="res-hero">
   <div class="sec-eye">{{ pageContent('resources', 'resources.hero_eyebrow') }}</div>
-  <h1>{{ pageContent('resources', 'resources.hero_heading') }}</h1>
+  <h1>{!! pageContent('resources', 'resources.hero_heading') !!}</h1>
   <p>{{ pageContent('resources', 'resources.hero_description') }}</p>
-</section>@include('partials.footer')
+</section>
+@include('partials.footer')
 @endsection
