9c0d436d8ec834c1d4fe638206d9867200f4c690
findkuldeepnath-crypto <findkuldeepnath@gmail.com>
2026-06-09 17:45:49 +0530

    test

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -43,19 +43,25 @@
 </section>
 
 {{-- DOCS SECTION --}}
-<section class="res-section section-white" id="docs">
 @foreach(pageContentJson('resources', 'resources.documentation') as $res_doc)
-  <div class="sec-eye">{{  $res_doc['sec_eye']  }}</div>
+<section class="res-section section-white" id="docs">
+  <div class="sec-eye">{{ $res_doc['sec_eye'] }}</div>
   <h2 class="sec-h2">{{ $res_doc['head'] }}</h2>
-  <p class="sec-sub">{{  $res_doc['description']  }}</p>
+  <p class="sec-sub">{{ $res_doc['description'] }}</p>
+
   <div class="res-doc-grid">
     @foreach($res_doc['list'] as $card)
-    <a href="{{ $card['link_url'] }}" class="res-doc-card">
-      <div class="res-doc-icon cyan"><i class="ti {{ $card['icon'] }}"></i></div>
-      <h4>{{ $card['title'] }}</h4>
-      <p>{{ $card['description'] }}</p>
-      <span class="res-link">{{ $card['link_text'] }} <i class="ti ti-arrow-right"></i></span>
-    </a>
+      <a href="{{ $card['link_url'] }}" class="res-doc-card">
+        <div class="res-doc-icon cyan">
+          <i class="ti {{ $card['icon'] }}"></i>
+        </div>
+        <h4>{{ $card['title'] }}</h4>
+        <p>{{ $card['list_description'] }}</p>
+        <span class="res-link">
+          {{ $card['link_text'] }}
+          <i class="ti ti-arrow-right"></i>
+        </span>
+      </a>
     @endforeach
   </div>
 </section>
