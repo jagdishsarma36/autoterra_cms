3ce4cb0a717f31eaa98edade9441a5c451c27a9a
findkuldeepnath-crypto <findkuldeepnath@gmail.com>
2026-06-09 13:13:32 +0530

    css

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -1,11 +1,39 @@
 @extends('layouts.app')
 @section('title', 'AutoTerra')
+
 @section('body')
 @include('partials.nav')
+
 <section class="res-hero">
-  <div class="sec-eye">{{ pageContent('resources', 'resources.hero_eyebrow') }}</div>
-  <h1>{!! pageContent('resources', 'resources.hero_heading') !!}</h1>
-  <p>{{ pageContent('resources', 'resources.hero_description') }}</p>
+  <div class="sec-eye">
+    {{ pageContent('resources', 'resources.hero_eyebrow') }}
+  </div>
+
+  <h1>
+    {!! pageContent('resources', 'resources.hero_heading') !!}
+  </h1>
+
+  <p>
+    {{ pageContent('resources', 'resources.hero_description') }}
+  </p>
 </section>
+
+@php
+  $buttons = json_decode(pageContent('resources', 'resources.buttons'), true);
+@endphp
+
+<section class="res-tabs" id="resTabs">
+  @if(!empty($buttons))
+    @foreach($buttons as $index => $btn)
+      <button 
+        class="res-tab {{ $index === 0 ? 'active' : '' }}"
+        onclick="scrollToRes('{{ trim($btn['link_url']) }}', this)">
+        <i class="ti {{ $btn['icon'] }}"></i>
+        {{ trim($btn['tsxt']) }}
+      </button>
+    @endforeach
+  @endif
+</section>
+
 @include('partials.footer')
 @endsection
