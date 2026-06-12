c5b271f4f3e3f2629a56bc0f214bd774f23036cd
jayantatamuli86-create <jayantatamuli86@gmail.com>
2026-06-12 19:03:05 +0530

    test

--- resources/views/pages/quote.blade.php
+++ resources/views/pages/quote.blade.php
@@ -115,7 +115,7 @@
           <div class="qt-sec-num">2</div>
           <div class="qt-sec-label">Preferred subscription term</div>
         </div>
-        <!-- <p class="qt-sec-des">
+        {{-- <p class="qt-sec-des">
           {{ $quote['subscription_term']['description'] }}
         </p>
         <div class="term-pills" id="termPills">
@@ -137,8 +137,8 @@
                 </span>
             @endif
         </label>
-        @endforeach -->
-      </div>
+        @endforeach
+      </div>  --}}
 
     <hr class="qt-sec-divider">
 
