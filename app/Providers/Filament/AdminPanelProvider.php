4f85181643b931743afb4022ae949b6ad4c50769
jayantatamuli86-create <jayantatamuli86@gmail.com>
2026-07-08 13:12:03 +0530

    adminlog

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->logoutRedirect(route('home'))
             ->colors([
                 'primary' => Color::Amber,
             ])
