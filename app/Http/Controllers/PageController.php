fa1750f6c0df1805880681952aa1175d4f93596b
shas1988 <shas.sarma@gmail.com>
2026-07-30 13:15:00 +0530

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -134,7 +134,7 @@ class PageController extends Controller
 
     public function cmsPagePreview(string $slug)
     {   
-        dump(Auth::user()->hasRole('admin'));
+        dump(Auth::user());
         if (!Auth::check()) {
             abort(403);
         }
