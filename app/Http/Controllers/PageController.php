91d017cc743e9871aca9669d91c2d1b0200a6aa5
shas1988 <shas.sarma@gmail.com>
2026-07-30 11:16:15 +0530

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -108,7 +108,6 @@ class PageController extends Controller
 
     public function dynamicPage(PageCms $page)
     {
-        dump($page);
         if (!$page->is_published || ($page->published_at && $page->published_at->isFuture())) {
             abort(404);
         }
