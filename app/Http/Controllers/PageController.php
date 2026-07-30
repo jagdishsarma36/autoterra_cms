a875f41706948b6033dccca0d171db6a7cfa258f
shas1988 <shas.sarma@gmail.com>
2026-07-30 11:49:49 +0530

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -135,14 +135,9 @@ class PageController extends Controller
     public function cmsPagePreview(string $slug)
     {
         $page = PageCms::findOrFail($slug);
-       
-        //dump($page);
         if (!$page) {
             abort(404);
         }
-        //if (!$page->is_published || ($page->published_at && $page->published_at->isFuture())) {
-            //abort(404);
-        //}
         return view('pages.dynamic-page', compact('page'));
     }
 
