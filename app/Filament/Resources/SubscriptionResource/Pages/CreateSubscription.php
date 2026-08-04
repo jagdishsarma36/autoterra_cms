842020e6beb109a9d5937eef84d52897c3bee908
shas1988 <shas.sarma@gmail.com>
2026-08-04 11:46:53 +0530

    Update CreateSubscription.php

--- app/Filament/Resources/SubscriptionResource/Pages/CreateSubscription.php
+++ app/Filament/Resources/SubscriptionResource/Pages/CreateSubscription.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\SubscriptionResource\Pages;
 
 use App\Filament\Resources\SubscriptionResource;
 use Filament\Resources\Pages\CreateRecord;
+use App\Mail\SubscriptionConfirmation;
 
 class CreateSubscription extends CreateRecord
 {
