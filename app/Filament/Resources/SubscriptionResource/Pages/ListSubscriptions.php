fcf1dcb4084f0f35cc02a5d55388f99d5ec8a78f
shas1988 <shas.sarma@gmail.com>
2026-08-04 11:41:19 +0530

    Update ListSubscriptions.php

--- app/Filament/Resources/SubscriptionResource/Pages/ListSubscriptions.php
+++ app/Filament/Resources/SubscriptionResource/Pages/ListSubscriptions.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\ListRecords;
 class ListSubscriptions extends ListRecords
 {
     protected static string $resource = SubscriptionResource::class;
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
