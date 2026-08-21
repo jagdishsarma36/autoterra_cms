72c200b327063085a6b22d578903e336869479bf
shas1988 <shas.sarma@gmail.com>
2026-08-21 10:27:50 +0530

    Update dynamic-page.blade.php

--- resources/views/pages/dynamic-page.blade.php
+++ resources/views/pages/dynamic-page.blade.php
@@ -241,7 +241,7 @@
     @if($rType === 'html')
       {!! $rValue !!}
     @elseif($rType === 'html_inline' || $rType === 'richtext')
-      <div class="page-content">{!! $rValue !!}</div>
+     <section class="section section-light"> <div class="page-content">{!! $rValue !!}</div> </section>
     @elseif(str_starts_with($rType, 'html_section'))
     @php $sectionClass = substr($rType, 13) ?: 'section-white'; @endphp
     <section class="section {{ $sectionClass }}">
